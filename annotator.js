1aa81599c0e721d4bde9e35a03d8ef94894df57a
Colin Bradford <cjthehippo@gmail.com>
2024-03-19 16:29:59 -0700

    fixed scroll offset for static positioned elements

--- annotator.js
+++ annotator.js
@@ -1,5 +1,10 @@
 (() => {
 
+    /**
+     * TODO:
+     *  - remove target size from annotater
+     *  - deal with offset issue on some elements (happens with some links in the body of mdn)
+     */
     var annotater = {
         parent2child: new Map(),
         annotationParentClassName: 'annotator-container-tator',
@@ -24,7 +29,7 @@
             return this;
         },
 
-        hasTaters: function() {
+        hasTaters: function () {
             let stylesheet = document.getElementById(this.stylesheetId);
             let overlays = document.querySelectorAll(`.${this.annotationParentClassName}`);
             return stylesheet || overlays.length;
@@ -32,7 +37,7 @@
 
 
         /** from microlib */
-        getSelectorHelper: function(element) {
+        getSelectorHelper: function (element) {
             let selector = element.tagName;
             let selectorUniqueAttrs = ''; // id, class e.g. tagname#spots.dog
             let selectorAttrs = ''; // where css uses brackets e.g. tagname[data-dog-name="spots"]
@@ -50,7 +55,7 @@
          * Gets a CSS selector that uniquely selects this component
          * @param {HTMLElement} element 
          */
-        getSelector: function(element) {
+        getSelector: function (element) {
             let selector = this.getSelectorHelper(element);
             let currentElement = element;
             while (document.querySelectorAll(selector).length > 1 && currentElement !== document.body) {
@@ -68,7 +73,7 @@
             2. take a callback/function that returns an html element that annotates the content
             3. 
         */
-        removeTaters: function() {
+        removeTaters: function () {
             let stylesheet = document.getElementById(this.stylesheetId);
             let overlays = document.querySelectorAll(`.${this.annotationParentClassName}`);
             stylesheet.remove();
@@ -77,7 +82,7 @@
             }
         },
 
-        setup: function() {
+        setup: function () {
             // create stylesheet
             stylesheet = document.createElement('style');
             stylesheet.id = this.stylesheetId;
@@ -109,16 +114,22 @@
                 + ' bottom: 0;'
                 + ' left: 0;'
                 + ' right: 0;'
+                + ' pointer-events: none;'
+                + '}';
+            let overlayDescendantsRule =
+                `.${this.annotationParentClassName} * {`
+                + ' pointer-events: all;'
                 + '}';
             document.documentElement.style.position = 'relative!important';
             // add rules
             stylesheet.sheet.insertRule(targetSizeRule);
             stylesheet.sheet.insertRule(elementBoxRule);
             stylesheet.sheet.insertRule(overlayRule);
+            stylesheet.sheet.insertRule(overlayDescendantsRule);
             return this.exists = true;
         },
 
-        getClosestNonStaticPositionedElement: function(element) {
+        getClosestNonStaticPositionedElement: function (element) {
             let position = 'static';
             while (position === 'static' && element !== document.documentElement) {
                 element = element.parentElement;
@@ -134,44 +145,51 @@
          * @param {Array<HTMLElement>} listOfElements list of elements to annotate
          * @param {Function} callback parameter 1 = the element to annotate, parameter 2 = the closest parent element with a non-static position, must return an HTMLElement
          */
-        annotateElements: function(listOfElements, callback) {
+        annotateElements: function (listOfElements, callback) {
 
             // Organize list of elements by closest parent element that has a non-static CSS position
             for (const element of listOfElements) {
                 let [parent, positionType] = this.getClosestNonStaticPositionedElement(element);
                 if (!this.parent2child.has(parent)) {
-                    this.parent2child.set(parent, []);
+                    this.parent2child.set(parent, { position: positionType, children: [] });
                 }
-                this.parent2child.get(parent).push(element);
+                this.parent2child.get(parent).children.push(element);
             }
 
             // create the overlay and annotations
-            for (const [parent, children] of this.parent2child.entries()) {
+            for (const [parent, parentProperties] of this.parent2child.entries()) {
                 // create an overlay layer for the parent element
                 let overlay = this.makeOverlay(parent);
-                for (const child of children) {
+                
+                for (const child of parentProperties.children) {
                     // make annotation
+                    
                     let annotation = callback(child, parent);
-                    this.positionAnnotation(annotation, child, parent);
+                    this.positionAnnotation(annotation, child, parent, parentProperties.position);
                     overlay.appendChild(annotation);
-                    if (child.tagName === 'SUMMARY') console.log('annotating summary (summary, overlay)', child, annotation);
                 }
             }
         },
 
-        makeOverlay: function(element) {
+        makeOverlay: function (element) {
             let overlay = document.createElement('div');
             overlay.classList.add(this.annotationParentClassName);
             element.appendChild(overlay);
             return overlay;
         },
 
-        positionAnnotation: function(annotation, annotatedElement, annotatedElementParent) {
+        positionAnnotation: function (annotation, annotatedElement, annotatedElementParent, annotatedElementParentPosition) {
             let elementRect = annotatedElement.getBoundingClientRect();
             let parentRect = annotatedElementParent.getBoundingClientRect();
+            let [scrollXOffset, scrollYOffest] = [0, 0];
+            
+            if (annotatedElementParentPosition !== 'static') {
+                [scrollXOffset, scrollYOffest] = this.getScrollOffest(annotatedElement, annotatedElementParent);
+            }
             // set element box location
-            let top = elementRect.top - parentRect.top;
-            let left = elementRect.left - parentRect.left;
+
+            let top = elementRect.top - parentRect.top + scrollYOffest;
+            let left = elementRect.left - parentRect.left + scrollXOffset;
             annotation.style.top = top + 'px';
             annotation.style.left = left + 'px';
             annotation.style.width = elementRect.width + 'px';
@@ -182,12 +200,30 @@
             });
         },
 
+        getScrollOffest: function (element, boundingParent = document.documentElement) {
+            let scrollYOffest, scrollXOffset;
+
+            const getScroll = (el, scrollProperty) => {
+                while (el[scrollProperty] <= 0 && el !== boundingParent) {
+                    el = el.parentElement;
+                }
+                return el[scrollProperty]
+            }
+
+            scrollYOffest = getScroll(element, "scrollTop");
+            scrollXOffset = getScroll(element, "scrollLeft");
+            return [scrollXOffset, scrollYOffest];
+        },
+
         isFocusable: function (element, log = false) {
             // is not inert or contained within an inert element
             // tabindex = 0
             // is not contained within a closed details
+            // if contained within closed details, must be a summary
+            // also need to ensure that closed details with a summary 
+            // is excluded if that details is in another closed details.
             let isTabbable = element.tabIndex === 0;
-            let isInert = element.hasAttribute('inert');
+            // closest also checks the element, not just its ancestors
             let isInInert = element.closest('[inert]');
             let detailsStart = element;
             if (element.tagName === 'SUMMARY') {
@@ -198,14 +234,13 @@
                 }
                 detailsStart = parentDetails;
                 // closest closed details
-                
             }
             let closestClosedAncestorDetails = detailsStart.closest('details:not([open])');
             if (log) console.log(`isTabbable ${isTabbable} !isInert ${!isInInert} !isInInert ${!isInInert} isInOpenDetails ${!closestClosedAncestorDetails}`);
-            return isTabbable && !isInert && !isInInert && !closestClosedAncestorDetails;
+            return isTabbable && !isInInert && !closestClosedAncestorDetails;
         },
 
-        addStyleRule: function(rule) {
+        addStyleRule: function (rule) {
             let stylesheet = document.getElementById(this.stylesheetId);
         }
     }.init();
@@ -237,14 +272,13 @@
         focusableElements = [...focusableElements].reduce((prev, cur) => {
             let isFocusable = annotater.isFocusable(cur);
             if (isFocusable) prev.push(cur);
-            if (cur.tagName === 'SUMMARY') console.log('found summary: ', cur, isFocusable);
             return prev;
         }, []);
         /* the elements in focusableElements passed each, individually, to makeTargetSize() */
         annotater.annotateElements(focusableElements, makeTargetSize);
     }
 
-    
+
 
     const makeTargetSize = (element, parentElement) => {
         let elementBox = document.createElement('div');
