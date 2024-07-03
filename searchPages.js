68f3e292822c17af28e0d9b0d038acd2f8cc5751
Colin Bradford <cjthehippo@gmail.com>
2024-07-03 14:51:13 -0700

    added styles, updated funcitonality, currently capable

--- searchPages.js
+++ searchPages.js
@@ -1,3 +1,9 @@
+/**
+ * TODO:
+ *  - add comments
+ *  - add text warning about needing to interact
+ *  - with the multi-select
+ */
 (() => {
     const listboxId = 'bmk-pages-listbox';
     const comboboxId = 'bmk-pages-combobox';
@@ -14,6 +20,10 @@
     function main() {
         let options = [...pages.querySelectorAll('option')];
         let [label, pageSearch, optionsSearch] = makePagesCombobox(options);
+        pageSearch.addEventListener('focusin', () => toggleListbox(pageSearch, true));
+        pageSearch.addEventListener('blur', (e) => {
+            if (!optionsSearch.contains(e.relatedTarget)) toggleListbox(pageSearch, false)
+        });
         pageSearch.addEventListener('input', searchOptions);
         pageSearch.addEventListener('keydown', keyRouter);
         optionsSearch.addEventListener('keydown', optionKeyRouter);
@@ -34,7 +44,7 @@
             case 'ArrowDown':
             case 'Enter':
                 if (listbox.hidden) toggleListbox(combobox);
-                listbox.querySelector('[role="option"]').focus();
+                listbox.querySelector('[role="option"]:not([hidden])').focus();
                 return;
             case 'Escape':
                 if (!listbox.hidden) toggleListbox(combobox);
@@ -51,28 +61,85 @@
         let containerRule =
             `#${containerId} {`
             + ` position: absolute;`
-            + ` height: 7rem;`
             + ` width: 100vw;`
             + ` z-index: 100;`
             + ` background: white;`
+            + ` display: flex;`
+            + ` flex-direction: row;`
+            + ` justify-content: space-between;`
+            + ` align-items: center;`
+            + ` gap: 0.5rem;`
+            + ` padding: 0.5rem;`
+            + ` box-shadow: 0px 3px 8px -3px black;`
+            + ` border-bottom: 1px solid black;`
+            + ` top: 0;`
             + `}`;
         let comboboxWidgetRule =
             `#${comboboxWidgetId} {`
             + ` position: relative;`
+            + ` width: fit-content;`
+            + `}`;
+        let comboboxContainerBtnRule =
+            `#${containerId} button {`
+            + ` border: 1px solid black;`
+            + ` box-shadow: 0px 3px 8px -3px black;`
+            + ` display: flex;`
+            + ` justify-content: center;`
+            + ` padding: 0.5rem;`
+            + `}`;
+        let comboboxContainerBtnHoverRule =
+            `#${containerId} button:hover {`
+            + ` box-shadow: 0px 4px 9px -3px black;`
+            + ` background-color: #EFEFEF;`
             + `}`;
         let listboxRule =
             `#${listboxId} {`
             + ` position: absolute;`
             + ` bottom: 0;`
             + ` transform: translate(0, 100%);`
-            + ` max-height: 100vh;`
+            + ` max-height: calc(100vh - 5rem);`
             + ` z-index: 100;`
             + ` background: white;`
             + ` overflow-y: scroll;`
+            + ` border: 1px solid black;`
+            + `}`;
+        let listboxOptionRule =
+            `#${listboxId} [role="option"] {`
+            + ` border-bottom: 1px solid black;`
+            + ` padding: 0.3rem;`
+            + `}`
+        let listboxOptionSelectedRule =
+            `#${listboxId} [role="option"].selected {`
+            + ` font-weight: bold;`
+            + ` border-bottom: 1px solid black;`
+            + ` padding: 0.3rem;`
+            + `}`
+        let listboxOptionRule2 = 
+            `#${listboxId} [role="option"]:nth-child(even):not(:focus) {`
+            + ` background-color: #EFEFEF;`
+            + `}`
+        let listboxOptionFocusRule = 
+            `#${listboxId} [role="option"]:focus {`
+            + ` outline: none;`
+            + ` color: #EFEFEF;`
+            + ` background-color: #333;`
+            + `}`
+        let comboboxRule =
+            `#${comboboxId} {`
+            + ` border: 1px solid black;`
+            + ` min-height: 2rem;`
+            + ` margin-left: 0.5rem;`
             + `}`;
         styles.sheet.insertRule(containerRule);
         styles.sheet.insertRule(comboboxWidgetRule);
+        styles.sheet.insertRule(comboboxContainerBtnRule);
+        styles.sheet.insertRule(comboboxContainerBtnHoverRule);
         styles.sheet.insertRule(listboxRule);
+        styles.sheet.insertRule(listboxOptionRule);
+        styles.sheet.insertRule(listboxOptionRule2);
+        styles.sheet.insertRule(listboxOptionFocusRule);
+        styles.sheet.insertRule(listboxOptionSelectedRule);
+        styles.sheet.insertRule(comboboxRule);
     }
 
     function makeComboboxWidget(label, combobox, listbox) {
@@ -85,8 +152,8 @@
     function close(element) {
         let container = element.closest('[role="region"]');
         option2option = {};
-        container.parentElement.removeChild(container);
-        styles.parentElement.removeChild(styles);
+        container.remove();
+        styles.remove();
         pages.focus();
     }
 
@@ -113,7 +180,7 @@
         );
         let currentOption = e.target;
         let options = Array.from(
-            listbox.querySelectorAll('[role="option"]')
+            listbox.querySelectorAll('[role="option"]:not([hidden])')
         );
         let currentIndex = options.indexOf(currentOption);
         let nextFocus, nextIndex;
@@ -144,8 +211,14 @@
 
     function activateOption(option) {
         let realOption = option2option[option.textContent];
-        console.log(realOption, option);
-        realOption.scrollIntoView();
+        //realOption.scrollIntoView({ block: 'center' });
+        realOption.style.fontWeight = '700';
+        realOption.selected = true;
+        realOption.click();
+        setTimeout(() => realOption.scrollIntoView({ block: 'center' }), 50);
+        setTimeout(() => {
+            realOption.style.fontWeight = 'normal';
+        }, 10000);
         close(option);
     }
 
@@ -172,18 +245,28 @@
             newOption.role = 'option';
             newOption.tabIndex = -1;
             newOption.textContent = o.textContent;
+            if (o.selected) {
+                newOption.textContent = newOption.textContent + ' (selected)';
+                newOption.classList.add('selected');
+            }
             option2option[newOption.textContent] = o;
+            newOption.addEventListener('click', () => {
+                activateOption(newOption);
+            });
             return newOption;
         }));
         return [label, combobox, listbox];
     }
 
     // return: the current expanded state
-    function toggleListbox(combobox) {
+    function toggleListbox(combobox, visibility) {
         let listbox = document.getElementById(
             combobox.getAttribute('aria-controls')
         );
         let expanded = combobox.getAttribute('aria-expanded') === 'true';
+        if (visibility !== undefined) {
+            expanded = !visibility;
+        }
         listbox.hidden = expanded;
         combobox.setAttribute('aria-expanded', !expanded);
         return !expanded;
@@ -198,7 +281,7 @@
         );
         for (let option of options) {
             let matched =
-                o.textContent.toLowerCase()
+            option.textContent.toLowerCase()
                     .includes(combobox.value.toLowerCase());
             option.hidden = !matched;
         }
